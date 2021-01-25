55ba001df3c97f108fa8dfff5c43cc830fa739c0
YuQing <1123095674@qq.com>
2021-01-25 17:02:26 +0800

    xiaoshi分支分类功能,底部导航

---
+++ pages/category/category.vue
@@ -0,0 +1,65 @@
+<template>
+	<view>
+		<u-search class="search" 
+		placeholder="日照香炉生紫烟" 
+		v-model="keyword" 
+		:clearabled="true" 
+		border-color="orange"
+		bg-color="white"></u-search>
+		
+		<view class="contentContainer">
+			<scroll-view scroll-y="true" class="leftNavScroll">
+				<view class="leftNavItem">1111</view>
+				<view class="leftNavItem">1111</view>
+				<view class="leftNavItem">1111</view>
+				<view class="leftNavItem">1111</view>
+				<view class="leftNavItem">1111</view>
+				<view class="leftNavItem">1111</view>
+				<view class="leftNavItem">1111</view>
+				<view class="leftNavItem">1111</view>
+				<view class="leftNavItem">1111</view>
+				<view class="leftNavItem">1111</view>
+				<view class="leftNavItem">1111</view>
+				<view class="leftNavItem">1111</view>
+				<view class="leftNavItem">1111</view>
+				<view class="leftNavItem">1111</view>
+				<view class="leftNavItem">1111</view>
+				<view class="leftNavItem">1111</view>
+				<view class="leftNavItem">1111</view>
+				<view class="leftNavItem">1111</view>
+				<view class="leftNavItem">1111</view>
+				<view class="leftNavItem">1111</view>
+				
+			</scroll-view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				data:"阿巴阿巴"
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.search{
+		padding: 20upx;
+	}
+	.contentContainer{
+		width: 100%;
+		margin-top: 20rpx;
+	}
+	.leftNavScroll{
+		width: 20%;
+		background-color: pink;
+		padding: 10rpx;
+	}
+	.leftNavScroll .leftNavItem{
+		padding: 8rpx;
+		
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,6 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+	<view class="wrap">
+		<u-swiper :list="pictures"></u-swiper>
 	</view>
 </template>
 
@@ -11,42 +8,25 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				pictures: [{
+							image: 'https://cdn.uviewui.com/uview/swiper/1.jpg',
+							title: '昨夜星辰昨夜风，画楼西畔桂堂东'
+						},
+						{
+							image: 'https://cdn.uviewui.com/uview/swiper/2.jpg',
+							title: '身无彩凤双飞翼，心有灵犀一点通'
+						},
+						{
+							image: 'https://cdn.uviewui.com/uview/swiper/3.jpg',
+							title: '谁念西风独自凉，萧萧黄叶闭疏窗，沉思往事立残阳'
+						}
+					],
 			}
 		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
 	}
 </script>
-
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+<style lang="scss">
+		.wrap {
+			padding: 40rpx;
+		}
 </style>
